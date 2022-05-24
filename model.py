3b8df38d58b787b177047e490eb270b46d9f9780
Thorsten Kober <thorsten.kober@gmail.com>
2022-05-24 21:43:15 +0200

    Added class template for the network

---
+++ model.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Network(nn.Module):
+
+    def __init__(self):
+        super(Network, self).__init__()
+        # TODO: Implement
+
+    def forward(self, state):
+        # TODO: Implement
+        pass
